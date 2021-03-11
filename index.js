757c39a40387e522c7dde0dd9b8ed4ec1ca208f9
Dani JM <xdanielmtz@gmail.com>
2021-03-10 19:42:51 -0500

    started the ajax call

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key="+key;
+var key ="XNFgr5MPnPiplHbUlgAaiaUD6hpy06jb";
+$.ajax({
+    URL: queryURL,
+    method: "GET"
+}).then()
+
+
+
 
 
 })
