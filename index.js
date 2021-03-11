b3b55057e929cbaa263fc2d8cc308f62a0ccf3c5
Dani JM <xdanielmtz@gmail.com>
2021-03-10 19:55:54 -0500

    created an on click event for the submit button

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key="+key;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&api-key="+key;
+var searchTerm = ""
 var key ="XNFgr5MPnPiplHbUlgAaiaUD6hpy06jb";
+
+
+$("#submitBtn").on("click", function (e) {
+    e.preventDefault()
+    console.log("works")
+
+
+
+
+    
+})
 $.ajax({
     URL: queryURL,
     method: "GET"
